00962bafeabf0b7f88eb5c786e3714c737a23fa6
rofiqulkaido <147033891+rofiqulkaido@users.noreply.github.com>
2023-10-15 14:27:48 +0700

    Update praktikum 6 & tugas 3 (recyclerview)
    Update praktikum 6 & tugas 3 (recyclerview)

--- app/src/main/java/com/example/rofiqul2612/FairyFragment.kt
+++ app/src/main/java/com/example/rofiqul2612/FairyFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,21 +22,44 @@ class FairyFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var rvBuku: RecyclerView? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //set layout manager di recyclerview
+        rvBuku?.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL,false)
+
+        //list data buku
+        val data = ArrayList<BukuModel>()
+        data.add(BukuModel(R.drawable.buku1, "Super Birthday Party",
+            "Lorem Ipsum Sit Amet"))
+        data.add(BukuModel(R.drawable.buku2, "Reach For The Sky",
+            "Lorem Ipsum Sit Amet"))
+        data.add(BukuModel(R.drawable.buku3, "Happy World Animals Day",
+            "Lorem Ipsum Sit Amet"))
+        data.add(BukuModel(R.drawable.buku4, "Learn & Play",
+            "Lorem Ipsum Sit Amet"))
+        data.add(BukuModel(R.drawable.buku5, "Back To School",
+            "Lorem Ipsum Sit Amet"))
+        data.add(BukuModel(R.drawable.buku6, "Dedicated to Reading",
+            "Lorem Ipsum Sit Amet"))
+
+        //set adapter
+        val adapter = AdapterHome(data)
+
+        //set adapter ke recycler view
+        rvBuku?.adapter = adapter
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_fairy, container, false)
+        rvBuku = view.findViewById(R.id.recyclerViewFairy)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fairy, container, false)
+        return view
     }
 
     companion object {
